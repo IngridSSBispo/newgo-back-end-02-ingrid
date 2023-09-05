024315042b537ce8c72db6969e5f3d660a4ba32c
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-05 16:55:13 -0300

    DOCS(db.sql) documentação do db

--- desafio-back-end-02-ingrid/src/db/db.sql
+++ desafio-back-end-02-ingrid/src/db/db.sql
@@ -23,3 +23,10 @@ SELECT * from produtos;
 INSERT INTO public.produtos(
 	 hash, nome, descricao, ean13, preco, quantidade, estoque_min, dtcreate, dtupdate, lativo)
 	VALUES ( 'd2d49a6c-4c1a-11ee-be56-0242ac120002', 'chocolate', 'barra de chocolate', 123456, 10.0, 5, 1 , now(), null, true);
+
+
+	ALTER TABLE produtos ALTER COLUMN preco TYPE numeric(13,2);
+
+	ALTER TABLE produtos ALTER COLUMN quantidade TYPE numeric(13,2);
+
+	ALTER TABLE produtos RENAME COLUMN id TO id_produto;
