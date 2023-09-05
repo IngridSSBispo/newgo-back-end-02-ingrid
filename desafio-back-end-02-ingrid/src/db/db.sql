ab7ddb77a811b7e1a85721fa0e7028875ef21e7b
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-05 16:11:12 -0300

    DOCS(db.sql) documentação do db

---
+++ desafio-back-end-02-ingrid/src/db/db.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS public.produtos
+(
+    id serial,
+    hash uuid,
+    nome character varying(255) COLLATE pg_catalog."default",
+    descricao text COLLATE pg_catalog."default",
+    ean13 character varying(13) COLLATE pg_catalog."default",
+    preco numeric,
+    quantidade numeric,
+    estoque_min numeric,
+    dtcreate timestamp with time zone,
+    dtupdate timestamp with time zone,
+    lativo boolean
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.produtos
+    OWNER to postgres;
+
+SELECT * from produtos;
+
+INSERT INTO public.produtos(
+	 hash, nome, descricao, ean13, preco, quantidade, estoque_min, dtcreate, dtupdate, lativo)
+	VALUES ( 'd2d49a6c-4c1a-11ee-be56-0242ac120002', 'chocolate', 'barra de chocolate', 123456, 10.0, 5, 1 , now(), null, true);
