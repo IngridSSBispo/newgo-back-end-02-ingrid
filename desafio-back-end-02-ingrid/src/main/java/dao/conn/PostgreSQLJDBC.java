7a680de6a7ccf3f4326e967a3bb141829501497b
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-08 12:35:39 -0300

    FEAT(PostgreSQLJDBC) adicionando classe de conexao com banco de dados

---
+++ desafio-back-end-02-ingrid/src/main/java/dao/conn/PostgreSQLJDBC.java
@@ -0,0 +1,31 @@
+package dao.conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgreSQLJDBC {
+    public static Connection getConnection () throws SQLException, ClassNotFoundException {
+        Connection conection  = null;
+
+        try{
+            Class.forName("org.postgresql.Driver");
+            conection = DriverManager
+                    .getConnection(
+                            "jdbc:postgresql://localhost:5432/estoque",
+                            "postgres",
+                            "g4m3g4m3");
+
+            return conection;
+
+        } catch(Exception e){
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            throw(e);
+
+        }
+
+//        System.out.println("Conexão inicializada com sucesso!");
+
+    }
+}
