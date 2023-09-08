e129bc875640cd8bf56c3af38f55242f4a521b5a
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-08 12:39:22 -0300

    FEAT(PrudutoDAO) adicionando método que lista os produtos.

--- desafio-back-end-02-ingrid/src/main/java/dao/ProdutoDAO.java
+++ desafio-back-end-02-ingrid/src/main/java/dao/ProdutoDAO.java
@@ -1,6 +1,38 @@
 package dao;
 
+import dao.conn.PostgreSQLJDBC;
+
+import java.sql.*;
+
 public class ProdutoDAO {
+
+
+    public String getAllProducts()  {
+        String result = "";
+       try {
+           Connection conn = PostgreSQLJDBC.getConnection();
+
+           conn.setAutoCommit(false);
+
+           Statement stmt = conn.createStatement();
+
+           ResultSet rs = stmt.executeQuery("select nome from produtos;");
+
+            while (rs.next()){
+                result = rs.getString("nome");
+            }
+
+
+           rs.close();
+           stmt.close();
+           conn.close();
+
+       } catch (Exception e){
+           e.printStackTrace();
+       }
+
+        return result;
+    }
 
 
 }
