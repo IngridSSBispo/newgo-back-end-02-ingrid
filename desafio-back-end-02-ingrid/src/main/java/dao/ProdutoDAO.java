33b7c35162a5c4b4fb4362f3f310fd06c640cb69
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-08 15:18:37 -0300

    FEAT(ProdutoDAO) adicionando método que cria produto no banco.

--- desafio-back-end-02-ingrid/src/main/java/dao/ProdutoDAO.java
+++ desafio-back-end-02-ingrid/src/main/java/dao/ProdutoDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import dao.conn.PostgreSQLJDBC;
+import produtos.Produto;
 
 import java.sql.*;
 
@@ -34,5 +35,39 @@ public class ProdutoDAO {
         return result;
     }
 
+
+    public boolean createProduct(Produto produto){
+        boolean success = false;
+
+        try {
+            Connection conn = PostgreSQLJDBC.getConnection();
+
+
+            String sql = "INSERT INTO produtos( " +
+                    "hash, nome, descricao, ean13, preco, quantidade, estoque_min, lativo) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?); ";
+
+            PreparedStatement stmt = conn.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setObject(1, produto.getHash());
+            stmt.setString(2, produto.getNome());
+            stmt.setString(3, produto.getDescricao());
+            stmt.setString(4, produto.getEan13());
+            stmt.setDouble(5, produto.getPreco());
+            stmt.setInt(6,produto.getQuantidade());
+            stmt.setInt(7, produto.getEstoque_min());
+            stmt.setBoolean(8, produto.getAtivo());
+
+            stmt.executeUpdate();
+            
+            success = true;
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return success;
+    }
 
 }
