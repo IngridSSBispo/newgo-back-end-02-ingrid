d7521a3ea52e3093f029513d768847ad09cee809
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-12 16:07:02 -0300

    REFACTOR(CreateProduct) clean code

--- desafio-back-end-02-ingrid/src/main/java/br/com/desafiobackend/CreateProduct.java
+++ desafio-back-end-02-ingrid/src/main/java/br/com/desafiobackend/CreateProduct.java
@@ -14,6 +14,8 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+
+
 public class CreateProduct extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -26,10 +28,6 @@ public class CreateProduct extends HttpServlet {
         while ((line = reader.readLine()) != null) {
             sb.append(line);
         }
-        String requestBody = sb.toString();
-        //writer.println(requestBody);
-
-        //depois preciso colocar para post, mas não sei como.
 
         String nome = request.getParameter("nome");
         String descricao = request.getParameter("descricao");
@@ -37,16 +35,6 @@ public class CreateProduct extends HttpServlet {
         String preco = request.getParameter("preco");
         String quantidade = request.getParameter("quantidade");
         String estoque_min = request.getParameter("estoque_min");
-
-//        writer.println(nome);
-//        writer.println(descricao);
-//        writer.println(ean13);
-//        writer.println(preco);
-//        writer.println(quantidade);
-//        writer.println(estoque_min);
-
-
-
 
         Produto produto = new Produto(
                 nome,
@@ -58,15 +46,24 @@ public class CreateProduct extends HttpServlet {
         );
 
         ProdutoDAO produtodao = new ProdutoDAO();
-        boolean success = produtodao.createProduct(produto);
 
-        String result = new Gson().toJson(produto);
+        boolean existNome = produtodao.checkIfExists("nome", nome);
+        boolean existEan13 = produtodao.checkIfExists("ean13", ean13);
 
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+        if(existNome || existEan13){
+            writer.println("Produto ja foi cadastrado anteriormente!");
+        } else {
+            boolean success = produtodao.createProduct(produto);
+            String result = new Gson().toJson(produto);
 
-        writer.print(result);
-        writer.flush();
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            writer.print(result);
+            writer.flush();
+        }
+
+
 
     }
 
