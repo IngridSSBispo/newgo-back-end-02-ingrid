ea0837281479e7aff78af3eaa2d7e219f9dec8a7
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-13 14:48:13 -0300

    FEAT(ListProducts) alterando instanciação de produto

--- desafio-back-end-02-ingrid/src/main/java/br/com/desafiobackend/ListProducts.java
+++ desafio-back-end-02-ingrid/src/main/java/br/com/desafiobackend/ListProducts.java
@@ -1,32 +1,23 @@
 package br.com.desafiobackend;
 import com.google.gson.Gson;
+import dao.ProdutoDAO;
 import produtos.Produto;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class ListProducts extends HttpServlet {
 
     @Override
     protected void service( HttpServletRequest request, HttpServletResponse response) throws IOException {
-
         PrintWriter writer = response.getWriter();
-
-
-        Produto produto = new Produto(
-                "Sabão",
-                "sabão liquido",
-                "12345678978",
-                10.00,
-                1,
-                10
-        );
-
-
-        String result = new Gson().toJson(produto);
+        ProdutoDAO dao = new ProdutoDAO();
+        ArrayList<Produto> allProducts = dao.getAllProducts();
+        String result = new Gson().toJson(allProducts);
 
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
