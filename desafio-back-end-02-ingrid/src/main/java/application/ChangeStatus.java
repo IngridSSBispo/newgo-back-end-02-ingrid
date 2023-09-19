55dd0cc14e6b7e89febc0bedc1d878a9e34cdbd6
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-19 16:39:52 -0300

    feat: criando endpoint de alteracao status US003

--- desafio-back-end-02-ingrid/src/main/java/application/ChangeStatus.java
+++ desafio-back-end-02-ingrid/src/main/java/application/ChangeStatus.java
@@ -1,10 +1,17 @@
 package application;
 
+
+import com.google.gson.Gson;
+import infrastructure.ProdutoDAO;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
+import java.util.UUID;
+
 
 public class ChangeStatus extends HttpServlet {
 
@@ -21,6 +28,24 @@ public class ChangeStatus extends HttpServlet {
 
         PrintWriter writer = response.getWriter();
         writer.println("Metodo POST foi disparado!");
+
+        String lativo = request.getParameter("lativo");
+        String hash = request.getParameter("hash");
+        ProdutoDAO produtodao = new ProdutoDAO();
+
+
+
+
+        if(!lativo.isEmpty() && !hash.isEmpty()){
+            boolean lativoConvert = true;
+            UUID hashConvert  = UUID.fromString("6e5b6b7a-9911-41ed-8e41-46d14c87db49");
+
+            produtodao.changeStatus(hashConvert, lativoConvert);
+
+        } else {
+            writer.println("Parametros invalidos");
+        }
+
 
 
 
