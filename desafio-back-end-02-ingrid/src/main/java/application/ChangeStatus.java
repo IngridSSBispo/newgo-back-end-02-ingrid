c34cf56722922b51f89918bde959f7890519ac77
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-20 11:09:59 -0300

    refactor: alterando o código utilizando o dto e removendo o que nao sera utilizado

--- desafio-back-end-02-ingrid/src/main/java/application/ChangeStatus.java
+++ desafio-back-end-02-ingrid/src/main/java/application/ChangeStatus.java
@@ -1,54 +1,53 @@
 package application;
 
 
+import application.dto.ChangeStatusDTO;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import infrastructure.ProdutoDAO;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
 import java.util.UUID;
 
 
 public class ChangeStatus extends HttpServlet {
-
+    Gson gson = new GsonBuilder().serializeNulls().create();
     protected void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException {
-
         PrintWriter writer = response.getWriter();
         writer.println("Metodo get foi disparado!");
-
 
 
     }
 
     protected void doPost (HttpServletRequest request, HttpServletResponse response) throws IOException {
-
         PrintWriter writer = response.getWriter();
-        writer.println("Metodo POST foi disparado!");
 
-        String lativo = request.getParameter("lativo");
-        String hash = request.getParameter("hash");
-        ProdutoDAO produtodao = new ProdutoDAO();
+        try {
+            ProdutoDAO produtodao = new ProdutoDAO();
+            StringBuilder sb = new StringBuilder();
+            BufferedReader reader = request.getReader();
 
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
 
+            ChangeStatusDTO changeStatusDTO = gson.fromJson(sb.toString(), ChangeStatusDTO.class);
 
+            produtodao.changeStatus(changeStatusDTO);
 
-        if(!lativo.isEmpty() && !hash.isEmpty()){
-            boolean lativoConvert = true;
-            UUID hashConvert  = UUID.fromString("6e5b6b7a-9911-41ed-8e41-46d14c87db49");
-
-            produtodao.changeStatus(hashConvert, lativoConvert);
-
-        } else {
+        } catch (Exception e){
             writer.println("Parametros invalidos");
+            e.printStackTrace();
         }
-
-
-
 
     }
 
 }
+
+
