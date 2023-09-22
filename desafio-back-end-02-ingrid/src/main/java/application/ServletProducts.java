b4ac58c724782913cdc3b14ce321620f536cad1e
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-22 12:37:51 -0300

    refactor: alterando metodo delete da servlet

--- desafio-back-end-02-ingrid/src/main/java/application/ServletProducts.java
+++ desafio-back-end-02-ingrid/src/main/java/application/ServletProducts.java
@@ -1,4 +1,6 @@
 package application;
+import application.actions.Delete;
+import application.actions.List;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,8 +10,11 @@ import java.io.PrintWriter;
 public class ServletProducts extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //LER TODOS | GET    | /"SUA_COLECAO_NO_PLURAL"
         PrintWriter writer = response.getWriter();
-        writer.println("Aqui e o  metodo get");
+        List ListProducts = new List();
+
+        ListProducts.list(response, writer);
 
     }
 
@@ -20,9 +25,12 @@ public class ServletProducts extends HttpServlet {
     }
 
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        EXCLUIR   | DELETE | /"SUA_COLECAO_NO_PLURAL"/{:id/hash}
         PrintWriter writer = response.getWriter();
 
-        writer.println("Aqui e o  metodo delete");
+        Delete deleteProduct = new Delete();
+
+        deleteProduct.delete(request, response);
 
     }
 
@@ -39,11 +47,11 @@ public class ServletProducts extends HttpServlet {
 
 /*
 
-LER TODOS | GET    | /"SUA_COLECAO_NO_PLURAL"
+LER TODOS | GET    | /"SUA_COLECAO_NO_PLURAL" ok
 INSERIR   | POST   | /"SUA_COLECAO_NO_PLURAL"
 
 LER 1     | GET    | /"SUA_COLECAO_NO_PLURAL"/{:id/hash}
-EXCLUIR   | DELETE | /"SUA_COLECAO_NO_PLURAL"/{:id/hash}
+
 ATUALIZAR | PUT    | /"SUA_COLECAO_NO_PLURAL"/{:id/hash}
 
  */
