aff94a0b949117d1fcbdf1ed134f7882a4fa50f7
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-25 20:36:34 -0300

    refactor: removendo atributos errados

--- desafio-back-end-02-ingrid/src/main/java/application/dto/CreateDTO.java
+++ desafio-back-end-02-ingrid/src/main/java/application/dto/CreateDTO.java
@@ -5,23 +5,17 @@ public class CreateDTO {
     private String nome;
     private String descricao;
     private String ean13;
-    private String preco;
-    private String quantidade;
-    private String estoque_min;
-    private int estoue_min_convertido ;
-    private double preco_conertido ;
-    private int quantidade_convertida  ;
+    private int estoque_min;
+    private double preco;
+    private int quantidade;
 
-    public CreateDTO(String nome, String descricao, String ean13, String preco, String quantidade, String estoque_min, int estoue_min_convertido, double preco_conertido, int quantidade_convertida) {
+    public CreateDTO(String nome, String descricao, String ean13, double preco, int quantidade, int estoque_min) {
         this.nome = nome;
         this.descricao = descricao;
         this.ean13 = ean13;
         this.preco = preco;
         this.quantidade = quantidade;
         this.estoque_min = estoque_min;
-        this.estoue_min_convertido = estoue_min_convertido;
-        this.preco_conertido = preco_conertido;
-        this.quantidade_convertida = quantidade_convertida;
     }
 
     public String getNome() {
@@ -36,28 +30,16 @@ public class CreateDTO {
         return ean13;
     }
 
-    public String getPreco() {
-        return preco;
-    }
-
-    public String getQuantidade() {
-        return quantidade;
-    }
-
-    public String getEstoque_min() {
+    public int getEstoque_min() {
         return estoque_min;
     }
 
-    public int getEstoue_min_convertido() {
-        return estoue_min_convertido;
+    public double getPreco() {
+        return preco;
     }
 
-    public double getPreco_conertido() {
-        return preco_conertido;
-    }
-
-    public int getQuantidade_convertida() {
-        return quantidade_convertida;
+    public int getQuantidade() {
+        return quantidade;
     }
 }
 
