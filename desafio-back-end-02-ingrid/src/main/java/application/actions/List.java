4ee697084ea82a2e49e4d473bf371ee5a167e87f
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-25 15:42:32 -0300

    feat: alterando parametros do metodo list e utilizando DTO status

--- desafio-back-end-02-ingrid/src/main/java/application/actions/List.java
+++ desafio-back-end-02-ingrid/src/main/java/application/actions/List.java
@@ -1,17 +1,39 @@
 package application.actions;
 
+import application.dto.CreateDTO;
+import application.dto.StatusDTO;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import domain.Produto;
 import infrastructure.ProdutoDAO;
 import infrastructure.Status;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class List {
-    public void list(HttpServletResponse response, PrintWriter writer) {
+    public void list(HttpServletRequest request, HttpServletResponse response, PrintWriter writer) throws IOException {
         ProdutoDAO dao = new ProdutoDAO();
-        ArrayList<Produto> allProducts = dao.getProducts(Status.TODOS, null);
+
+
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        Gson gson = new GsonBuilder().serializeNulls().create();
+
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+
+        StatusDTO statusDTO = gson.fromJson(sb.toString(), StatusDTO.class);
+
+
+        ArrayList<Produto> allProducts = dao.getProducts(statusDTO.getStatus(), statusDTO.getHash());
         String result = new Gson().toJson(allProducts);
 
         response.setContentType("application/json");
