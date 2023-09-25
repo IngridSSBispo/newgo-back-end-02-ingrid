88e4b6aef27a8df798c5c17adf2934841e65ae8b
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-25 15:40:26 -0300

    feat: criando Status DTO para listar ativos e inativos

---
+++ desafio-back-end-02-ingrid/src/main/java/application/dto/StatusDTO.java
@@ -0,0 +1,24 @@
+package application.dto;
+
+import infrastructure.Status;
+
+import java.util.UUID;
+
+public class StatusDTO {
+
+    private Status status;
+    private UUID hash = null;
+
+    public StatusDTO(UUID hash, Status status) {
+        this.hash = hash;
+        this.status = status;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public UUID getHash() {
+        return hash;
+    }
+}
