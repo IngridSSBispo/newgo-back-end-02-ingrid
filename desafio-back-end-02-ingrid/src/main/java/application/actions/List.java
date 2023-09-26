0f00c61518d8079d9226e8bc979d40d80994fcb2
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-26 14:26:41 -0300

    feat: adicionando metodo listStock

--- desafio-back-end-02-ingrid/src/main/java/application/actions/List.java
+++ desafio-back-end-02-ingrid/src/main/java/application/actions/List.java
@@ -1,13 +1,12 @@
 package application.actions;
 
-import application.dto.CreateDTO;
+import application.dto.EstoqueMinDTO;
 import application.dto.StatusDTO;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import domain.Produto;
 import infrastructure.ProdutoDAO;
 import infrastructure.Status;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
@@ -19,11 +18,10 @@ public class List {
     public void list(HttpServletRequest request, HttpServletResponse response, PrintWriter writer) throws IOException {
         ProdutoDAO dao = new ProdutoDAO();
 
-
+        //pegando os parametros a partir do body request
         StringBuilder sb = new StringBuilder();
         BufferedReader reader = request.getReader();
         Gson gson = new GsonBuilder().serializeNulls().create();
-
 
         String line;
         while ((line = reader.readLine()) != null) {
@@ -32,8 +30,8 @@ public class List {
 
         StatusDTO statusDTO = gson.fromJson(sb.toString(), StatusDTO.class);
 
+        ArrayList<Produto> allProducts = dao.getProducts(statusDTO.getStatus(), statusDTO.getHash(), -1);
 
-        ArrayList<Produto> allProducts = dao.getProducts(statusDTO.getStatus(), statusDTO.getHash());
         String result = new Gson().toJson(allProducts);
 
         response.setContentType("application/json");
@@ -41,5 +39,37 @@ public class List {
 
         writer.print(result);
         writer.flush();
+    }
+
+    /*
+     * US006 - Criar recurso para consultar todos os produtos em quantidade de estoque
+     * menor que o  estoque mínimo definido.
+     */
+    public void listStock(HttpServletRequest request, HttpServletResponse response, PrintWriter writer) throws IOException {
+
+        ProdutoDAO dao = new ProdutoDAO();
+
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        Gson gson = new GsonBuilder().serializeNulls().create();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        //StatusDTO statusDTO = gson.fromJson(sb.toString(), StatusDTO.class);
+
+        EstoqueMinDTO estoqueMin = gson.fromJson(sb.toString(), EstoqueMinDTO.class);
+
+        ArrayList<Produto> allProducts = dao.getProducts(Status.TODOS, null, estoqueMin.getEstoqueMin());
+
+        String result = new Gson().toJson(allProducts);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        writer.print(result);
+        writer.flush();
+
     }
 }
