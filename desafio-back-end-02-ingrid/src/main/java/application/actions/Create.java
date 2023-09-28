c5129b8df84f1833f7e8c87b5fcfedd24abe0e15
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-27 23:41:35 -0300

    feat: permitindo cadastro em lote

--- desafio-back-end-02-ingrid/src/main/java/application/actions/Create.java
+++ desafio-back-end-02-ingrid/src/main/java/application/actions/Create.java
@@ -20,47 +20,50 @@ public class Create {
         BufferedReader reader = request.getReader();
         Gson gson = new GsonBuilder().serializeNulls().create();
 
-
         String line;
         while ((line = reader.readLine()) != null) {
             sb.append(line);
         }
 
-        CreateDTO createDTO = gson.fromJson(sb.toString(), CreateDTO.class);
+        CreateDTO[] createData = gson.fromJson(sb.toString(), CreateDTO[].class);
 
-        if (createDTO.getNome() == null || createDTO.getNome().isEmpty()) {
-            writer.println("O campo nome nao pode ser nulo ou vazio");
-        } else {
-            UUID hash = UUID.randomUUID();
-            Produto produto = new Produto(
-                    hash,
-                    createDTO.getNome(),
-                    createDTO.getDescricao(),
-                    createDTO.getEan13(),
-                    createDTO.getPreco(),
-                    createDTO.getQuantidade(),
-                    createDTO.getEstoque_min()
+        for(int i = 0; i < createData.length; i++) {
+            CreateDTO singleData = createData[i];
 
-            );
-
-            ProdutoDAO produtodao = new ProdutoDAO();
-
-            boolean existNome = produtodao.checkIfExists("nome", createDTO.getNome());
-            boolean existEan13 = produtodao.checkIfExists("ean13", createDTO.getEan13());
-
-            if (existNome || existEan13) {
-                writer.println("Produto ja foi cadastrado anteriormente!");
+            if (singleData.getNome() == null || singleData.getNome().isEmpty()) {
+                writer.println("O campo nome nao pode ser nulo ou vazio");
             } else {
-                boolean success = produtodao.createProduct(produto);
-                String result = new Gson().toJson(produto);
+                UUID hash = UUID.randomUUID();
+                Produto produto = new Produto(
+                        hash,
+                        singleData.getNome(),
+                        singleData.getDescricao(),
+                        singleData.getEan13(),
+                        singleData.getPreco(),
+                        singleData.getQuantidade(),
+                        singleData.getEstoque_min()
 
-                response.setContentType("application/json");
-                response.setCharacterEncoding("UTF-8");
+                );
 
-                writer.print(result);
-                writer.flush();
+                ProdutoDAO produtodao = new ProdutoDAO();
+
+                boolean existNome = produtodao.checkIfExists("nome", singleData.getNome());
+                boolean existEan13 = produtodao.checkIfExists("ean13", singleData.getEan13());
+
+                if (existNome || existEan13) {
+                    writer.println("Produto ja foi cadastrado anteriormente!");
+                } else {
+                    boolean success = produtodao.createProduct(produto);
+                    String result = new Gson().toJson(produto);
+
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("UTF-8");
+
+                    writer.print(result);
+                    writer.flush();
+                }
+
             }
-
         }
     }
 
