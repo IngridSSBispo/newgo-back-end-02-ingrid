a6a17d3131e40f658b10fd2bd160ad6e6e777ab2
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-09-28 15:18:25 -0300

    refactor: alterando o metodo create e adicionando create multiple para se adequar a US008 de atualizar em lote

--- desafio-back-end-02-ingrid/src/main/java/application/actions/Create.java
+++ desafio-back-end-02-ingrid/src/main/java/application/actions/Create.java
@@ -25,9 +25,60 @@ public class Create {
             sb.append(line);
         }
 
+        CreateDTO createDTO = gson.fromJson(sb.toString(), CreateDTO.class);
+
+        if (createDTO.getNome() == null || createDTO.getNome().isEmpty()) {
+            writer.println("O campo nome nao pode ser nulo ou vazio");
+        } else {
+            UUID hash = UUID.randomUUID();
+            Produto produto = new Produto(
+                    hash,
+                    createDTO.getNome(),
+                    createDTO.getDescricao(),
+                    createDTO.getEan13(),
+                    createDTO.getPreco(),
+                    createDTO.getQuantidade(),
+                    createDTO.getEstoque_min()
+
+
+            );
+
+            ProdutoDAO produtodao = new ProdutoDAO();
+
+            boolean existNome = produtodao.checkIfExists("nome", createDTO.getNome());
+            boolean existEan13 = produtodao.checkIfExists("ean13", createDTO.getEan13());
+
+            if (existNome || existEan13) {
+                writer.println("Produto ja foi cadastrado anteriormente!");
+            } else {
+                boolean success = produtodao.createProduct(produto);
+                String result = new Gson().toJson(produto);
+
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+
+                writer.print(result);
+                writer.flush();
+            }
+
+        }
+    }
+
+
+    public void createMultiple(HttpServletRequest request, HttpServletResponse response, PrintWriter writer) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        Gson gson = new GsonBuilder().serializeNulls().create();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+
         CreateDTO[] createData = gson.fromJson(sb.toString(), CreateDTO[].class);
 
-        for(int i = 0; i < createData.length; i++) {
+        for (int i = 0; i < createData.length; i++) {
             CreateDTO singleData = createData[i];
 
             if (singleData.getNome() == null || singleData.getNome().isEmpty()) {
